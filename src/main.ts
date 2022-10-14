d7c35af26ec5da0bda407cc83a00380d2f53f380
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-10-14 15:46:50 +0700

    build: implement logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { LogLevel } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const levels: LogLevel[] =
+    process.env.NOED_ENV === 'production'
+      ? ['error', 'log', 'warn']
+      : ['debug', 'error', 'log', 'warn', 'verbose'];
+
+  const app = await NestFactory.create(AppModule, {
+    logger: levels,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
